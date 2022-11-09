eb4c0211b82efbebaea1ea924c36df2c553649f8
Jennifer Reuting <jen@reuting.com>
2022-11-08 16:32:28 -0800

    Wrote test for add shipment route

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -22,4 +22,26 @@ describe("POST /", function () {
       .send();
     expect(resp.statusCode).toEqual(400);
   });
+
+  test("returns specific error messages if invalid data given", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send({
+        "productId": 10,
+        "name": "Tes",
+        "addr": "501 Main Street",
+        "zip": null
+      });
+      expect(resp.statusCode).toEqual(400);
+      expect(resp.body).toEqual({
+        "error": {
+          "message": [
+            "instance.productId must be greater than or equal to 1000",
+            "instance.name does not meet minimum length of 5",
+            "instance.zip is not of a type(s) string"
+          ],
+          "status": 400
+        }
+      });
+  })
 });
